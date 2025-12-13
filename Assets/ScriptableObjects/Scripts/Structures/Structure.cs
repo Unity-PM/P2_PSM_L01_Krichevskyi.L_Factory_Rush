d48260bdfd65ad38adfa54f8fe0211b758169dd9
urban-rgb <l.urban.work@gmail.com>
2025-12-13 15:14:51 +0100

    refactor: change ClassDiagram

---
+++ Assets/ScriptableObjects/Scripts/Structures/Structure.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Structure
+{
+    [SerializeField] protected int maxHp;
+    [SerializeField] protected int currHp;
+
+    [SerializeField] protected int direction;
+    [SerializeField] protected Vector2 space;
+    [SerializeField] protected Vector2 position;
+
+
+
+}
